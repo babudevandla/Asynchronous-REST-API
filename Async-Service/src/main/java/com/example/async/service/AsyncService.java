d38171e1114f33d339f1bba39079fe529d402e38
Babu Devandla <babu.devandla524@gmail.com>
2022-05-19 17:22:23 +0530

    Initial Committing

---
+++ Async-Service/src/main/java/com/example/async/service/AsyncService.java
@@ -0,0 +1,66 @@
+package com.example.async.service;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.async.model.EmployeeAddresses;
+import com.example.async.model.EmployeeNames;
+import com.example.async.model.EmployeePhone;
+
+@Service
+public class AsyncService {
+
+	private static Logger log = LoggerFactory.getLogger(AsyncService.class);
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Async("asyncExecutor")
+	public CompletableFuture<EmployeeNames> getEmployeeName() throws InterruptedException 
+	{
+		log.info("getEmployeeName Starts");
+		EmployeeNames employeeNameData = restTemplate.getForObject("http://localhost:8082/names", EmployeeNames.class);
+
+		log.info("employeeNameData, {}", employeeNameData);
+		Thread.sleep(1000L);	//Intentional delay
+		log.info("employeeNameData completed");
+		return CompletableFuture.completedFuture(employeeNameData);
+	}
+
+	@Async("asyncExecutor")
+	public CompletableFuture<EmployeeAddresses> getEmployeeAddress() throws InterruptedException 
+	{
+		log.info("getEmployeeAddress Starts");
+		EmployeeAddresses employeeAddressData = restTemplate.getForObject("http://localhost:8082/addresses", EmployeeAddresses.class);
+
+		log.info("employeeAddressData, {}", employeeAddressData);
+		Thread.sleep(1000L);	//Intentional delay
+		log.info("employeeAddressData completed");
+		return CompletableFuture.completedFuture(employeeAddressData);
+	}
+
+	@Async("asyncExecutor")
+	public CompletableFuture<EmployeePhone> getEmployeePhone() throws InterruptedException 
+	{
+		log.info("getEmployeePhone Starts");
+		EmployeePhone employeePhoneData = restTemplate.getForObject("http://localhost:8082/phones", EmployeePhone.class);
+
+		log.info("employeePhoneData, {}", employeePhoneData);
+		Thread.sleep(1000L);	//Intentional delay
+		log.info("employeePhoneData completed");
+		return CompletableFuture.completedFuture(employeePhoneData);
+	}
+
+}
